48c0818a44271eaedf869cc9462809cbdbc851b9
foxigor98@mail.ru <foxigor@foxigor.localdomain>
2022-04-26 14:48:37 +0300

    Улучшил внешний вид

--- comments_app/urls.py
+++ comments_app/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from .views import CommentDetailView, PostDetailView
 
 urlpatterns = [
-    # path('api/', show_genres),
-    # path('api/<genre>', show_one_genre),
     path('api/add_new_post', PostDetailView.as_view()),
     path('api/reply_to_post', CommentDetailView.as_view()),
     path('api/posts', PostDetailView.as_view()),
--- comments_app/views.py
+++ comments_app/views.py
@@ -1,32 +1,43 @@
-from .models import Post, Comment
-from django.http import JsonResponse
-from django.views import View
 import json
+
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import IntegrityError
-    
+from django.http import JsonResponse
+from django.views import View
+
+from .models import Post, Comment
+
+
 class CommentDetailView(View):
+
     def get(self, request):
         comment_id = request.GET.get('id')
         try:
-            comment = Comment.objects.get(pk = comment_id)
+            comment = Comment.objects.get(pk=comment_id)
         except ObjectDoesNotExist:
-            return JsonResponse({'text': 'Комментарий с указанным id не существует'}, status = 404)    
+            return JsonResponse(
+                {'text': 'Комментарий с указанным id не существует'},
+                status=404)
         return JsonResponse({'comment': comment.serialize_object()})
-        
+
     def post(self, request):
         post_body = json.loads(request.body)
         text = post_body.get('text')
-        try: 
-            post_id = Post.objects.get(pk = post_body['post_id'])
+        try:
+            post_id = Post.objects.get(pk=post_body['post_id'])
         except ObjectDoesNotExist:
-            return JsonResponse({'text': 'Пост с указанным id не существует'}, status = 404)
+            return JsonResponse(
+                {'text': 'Пост с указанным id не существует'},
+                status=404)
         parent = post_body.get('parent')
         if parent:
-            Comment.objects.create(text=text, post=post_id, parent = Comment.objects.get(pk=parent))
+            Comment.objects.create(text=text, post=post_id,
+                                   parent=Comment.objects.get(pk=parent))
         else:
             Comment.objects.create(text=text, post=post_id)
-        return JsonResponse({'text': 'Комментарий успешно создан'}, status = 201)
+        return JsonResponse({'text': 'Комментарий успешно создан'},
+                            status=201)
+
 
 class PostDetailView(View):
     def get(self, request):
@@ -34,11 +45,13 @@ class PostDetailView(View):
         try:
             posts = Post.objects.filter(title__contains=post_title)
         except ObjectDoesNotExist:
-            return JsonResponse({'text': 'Пост с указанным названием не существует'}, status = 404)
+            return JsonResponse(
+                {'text': 'Пост с указанным названием не существует'},
+                status=404)
         response = {}
         for post in posts:
             comments = []
-            for comment in Comment.objects.filter(post = post, level = 0):
+            for comment in Comment.objects.filter(post=post, level=0):
                 comments.append(comment.serialize_object(limit=2))
             post_response = {
                 'id': post.id,
@@ -46,14 +59,14 @@ class PostDetailView(View):
                 'text': post.text,
                 'comments': comments}
             response[post.title] = post_response
-        return JsonResponse(response, status = 200)    
+        return JsonResponse(response, status=200)
 
     def post(self, request):
         post_body = json.loads(request.body)
         try:
-            Post.objects.create(title=post_body['title'], text=post_body['text'])
+            Post.objects.create(title=post_body['title'],
+                                text=post_body['text'])
         except IntegrityError:
-            return JsonResponse({'text': 'Пост с таким названием уже существует'}, status = 400)
-        return JsonResponse({'text': 'Пост успешно создан'}, status = 201)
-
-
+            return JsonResponse(
+                {'text': 'Пост с таким названием уже существует'}, status=400)
+        return JsonResponse({'text': 'Пост успешно создан'}, status=201)
